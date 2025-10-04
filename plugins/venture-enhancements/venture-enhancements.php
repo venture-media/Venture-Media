b39d2b69a75d390770e28d36854b092289eb8409
Leon <150437885+Leon2332@users.noreply.github.com>
2025-10-04 16:18:59 +0200

    Create venture-enhancements.php
    Create plugin to move non-essential functions out of child theme.

---
+++ plugins/venture-enhancements/venture-enhancements.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: Venture Enhancements
+ * Plugin URI:  https://www.venture.com.na
+ * Description: Site-specific enhancements, non-essential features for Venture website.
+ * Version:     0.9.0-beta
+ * Author:      Leon de Klerk
+ * Author URI:  https://github.com/Leon2332
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * -----------------------------
+ * 1. Staff Dashboard Shortcodes
+ * -----------------------------
+ */
+require_once plugin_dir_path(__FILE__) . 'includes/staff-shortcodes.php';
+
+/**
+ * -----------------------------
+ * 2. WooCommerce Tweaks
+ * -----------------------------
+ */
+require_once plugin_dir_path(__FILE__) . 'includes/woocommerce-tweaks.php';
+
+/**
+ * -----------------------------
+ * 3. Front-end Enhancements
+ * -----------------------------
+ */
+require_once plugin_dir_path(__FILE__) . 'includes/frontend-enhancements.php';
+
+/**
+ * -----------------------------
+ * 4. Dashboard Memory Widget
+ * -----------------------------
+ */
+require_once plugin_dir_path(__FILE__) . 'includes/memory-widget.php';

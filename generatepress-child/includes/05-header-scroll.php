78b6226d08cefee7c0e36b3a6a0b7bc131119a37
Leon <150437885+Leon2332@users.noreply.github.com>
2025-10-08 14:46:31 +0200

    Create 05-header-scroll.php

---
+++ generatepress-child/includes/05-header-scroll.php
@@ -0,0 +1,7 @@
+<?php
+// outputs JS with dynamic logo URLs
+?>
+<script>
+  const logoBlack = "<?php echo esc_url( home_url('/wp-content/uploads/2025/09/venture-logo-black.svg') ); ?>";
+  const logoWhite = "<?php echo esc_url( home_url('/wp-content/uploads/2025/09/venture-logo-white.svg') ); ?>";
+</script>

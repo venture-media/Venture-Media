d79dbd3ce2803cde3cb90fbed4e67e712ea67f9c
Leon <150437885+Leon2332@users.noreply.github.com>
2025-10-08 14:50:14 +0200

    Update 05-header-scroll.php

--- generatepress-child/includes/05-header-scroll.php
+++ generatepress-child/includes/05-header-scroll.php
@@ -1,7 +1,8 @@
 <?php
 // outputs JS with dynamic logo URLs
-?>
 <script>
-  const logoBlack = "<?php echo esc_url( home_url('/wp-content/uploads/2025/09/venture-logo-black.svg') ); ?>";
-  const logoWhite = "<?php echo esc_url( home_url('/wp-content/uploads/2025/09/venture-logo-white.svg') ); ?>";
+  window.VentureLogo = {
+    black: "<?php echo esc_url( home_url('/wp-content/uploads/2025/09/venture-logo-black.svg') ); ?>",
+    white: "<?php echo esc_url( home_url('/wp-content/uploads/2025/09/venture-logo-white.svg') ); ?>"
+  };
 </script>

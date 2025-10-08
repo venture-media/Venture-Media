a5dd7e2b33b7bc0e5c4d36c282c18a8b62032346
Leon <150437885+Leon2332@users.noreply.github.com>
2025-10-08 15:05:18 +0200

    Delete generatepress-child/includes/05-header-scroll.php

--- generatepress-child/includes/05-header-scroll.php
+++
@@ -1,8 +0,0 @@
-<?php
-// outputs JS with dynamic logo URLs
-<script>
-  window.VentureLogo = {
-    black: "<?php echo esc_url( home_url('/wp-content/uploads/2025/09/venture-logo-black.svg') ); ?>",
-    white: "<?php echo esc_url( home_url('/wp-content/uploads/2025/09/venture-logo-white.svg') ); ?>"
-  };
-</script>

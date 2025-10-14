a6578e200ef9894dba8d2d2e801b52cf97112539
Leon <150437885+Leon2332@users.noreply.github.com>
2025-10-14 22:17:41 +0200

    Create 07-charts.php

---
+++ generatepress-child/includes/07-charts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * -----------------------------
+ * 07 Charts
+ * -----------------------------
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function my_register_report_chart_widget( $widgets_manager ) {
+    require_once get_stylesheet_directory() . '/elementor-widgets/class-report-chart-widget.php';
+    $widgets_manager->register( new \Report_Chart_Widget() );
+}
+add_action( 'elementor/widgets/register', 'my_register_report_chart_widget' );

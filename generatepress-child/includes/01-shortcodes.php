adb58f0469be204f1090088f433ddafff3943d10
Leon <150437885+Leon2332@users.noreply.github.com>
2025-10-14 21:22:46 +0200

    Update and rename 01-staff-shortcodes.php to 01-shortcodes.php

--- generatepress-child/includes/01-shortcodes.php
+++ generatepress-child/includes/01-shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * -----------------------------
- * 01 Staff Dashboard Shortcodes
+ * 01 Shortcodes
  * -----------------------------
  */
 
@@ -336,3 +336,62 @@ function staff_directory_shortcode() {
     return ob_get_clean();
 }
 add_shortcode('staff_directory', 'staff_directory_shortcode');
+
+
+
+// Shortcode to display an Elementor template
+function my_elementor_template_shortcode( $atts ) {
+    $atts = shortcode_atts( [
+        'id' => '', // Elementor template ID
+    ], $atts, 'elementor_template' );
+
+    if ( empty( $atts['id'] ) ) {
+        return ''; // no ID provided
+    }
+
+    return \Elementor\Plugin::instance()->frontend->get_builder_content_for_display( $atts['id'] );
+}
+add_shortcode( 'elementor_template', 'my_elementor_template_shortcode' );
+
+
+
+// Allow shortcodes in menu item titles
+add_filter( 'wp_nav_menu_items', function( $items, $args ) {
+    return do_shortcode( $items );
+}, 10, 2 );
+
+
+
+function vv_last_updated_shortcode( $atts ) {
+    global $post;
+
+    $atts = shortcode_atts( array(
+        'format'       => get_option( 'date_format' ),
+        'label'        => '',
+        'show_time'    => 'false',
+        'hide_if_same' => 'false',
+        'class'        => 'last-updated-date', // default CSS class
+    ), $atts, 'last_updated' );
+
+    $post_id = ( is_object( $post ) && isset( $post->ID ) ) ? $post->ID : get_the_ID();
+    if ( ! $post_id ) {
+        return '';
+    }
+
+    $format = $atts['format'];
+    if ( strtolower( $atts['show_time'] ) === 'true' ) {
+        $format = $format . ' ' . get_option( 'time_format' );
+    }
+
+    $modified  = get_the_modified_date( $format, $post_id );
+    $published = get_the_date( $format, $post_id );
+
+    if ( strtolower( $atts['hide_if_same'] ) === 'true' && $modified === $published ) {
+        return '';
+    }
+
+    $output = esc_html( $atts['label'] . $modified );
+
+    return '<span class="' . esc_attr( $atts['class'] ) . '">' . $output . '</span>';
+}
+add_shortcode( 'last_updated', 'vv_last_updated_shortcode' );
